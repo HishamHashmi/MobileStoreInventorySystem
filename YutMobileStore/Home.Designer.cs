718348a8f5097774ae07e473702a94fb8e8b76a0
Hisham Hashmi <hishamhashmi@outlook.com>
2019-08-20 21:29:54 +0500

    Add project files.

---
+++ YutMobileStore/Home.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace YutMobileStore
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogOut = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.ProductInfo = new System.Windows.Forms.Button();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.Reports = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.Attendance = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.Stock = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.Sale = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.Ledger = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Accessories = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Phones = new System.Windows.Forms.Button();
+            this.InventoryPanel = new System.Windows.Forms.Panel();
+            this.Inventory = new System.Windows.Forms.Button();
+            this.panel9.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.InventoryPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LogOut
+            // 
+            this.LogOut.BackColor = System.Drawing.Color.Black;
+            this.LogOut.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LogOut.FlatAppearance.BorderSize = 0;
+            this.LogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LogOut.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogOut.ForeColor = System.Drawing.Color.White;
+            this.LogOut.Location = new System.Drawing.Point(1256, 1);
+            this.LogOut.Name = "LogOut";
+            this.LogOut.Size = new System.Drawing.Size(110, 45);
+            this.LogOut.TabIndex = 2;
+            this.LogOut.Text = "LogOut";
+            this.LogOut.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Candara", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(514, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 59);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Main Menu";
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.White;
+            this.panel9.BackgroundImage = global::YutMobileStore.Properties.Resources.products;
+            this.panel9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel9.Controls.Add(this.ProductInfo);
+            this.panel9.Location = new System.Drawing.Point(779, 534);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(206, 153);
+            this.panel9.TabIndex = 10;
+            // 
+            // ProductInfo
+            // 
+            this.ProductInfo.BackColor = System.Drawing.Color.Black;
+            this.ProductInfo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ProductInfo.FlatAppearance.BorderSize = 0;
+            this.ProductInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProductInfo.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProductInfo.ForeColor = System.Drawing.Color.White;
+            this.ProductInfo.Location = new System.Drawing.Point(25, 93);
+            this.ProductInfo.Name = "ProductInfo";
+            this.ProductInfo.Size = new System.Drawing.Size(157, 40);
+            this.ProductInfo.TabIndex = 3;
+            this.ProductInfo.Text = "Product Info";
+            this.ProductInfo.UseVisualStyleBackColor = false;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.White;
+            this.panel8.BackgroundImage = global::YutMobileStore.Properties.Resources.reports;
+            this.panel8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel8.Controls.Add(this.Reports);
+            this.panel8.Location = new System.Drawing.Point(549, 534);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(206, 153);
+            this.panel8.TabIndex = 9;
+            // 
+            // Reports
+            // 
+            this.Reports.BackColor = System.Drawing.Color.Black;
+            this.Reports.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Reports.FlatAppearance.BorderSize = 0;
+            this.Reports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Reports.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Reports.ForeColor = System.Drawing.Color.White;
+            this.Reports.Location = new System.Drawing.Point(25, 93);
+            this.Reports.Name = "Reports";
+            this.Reports.Size = new System.Drawing.Size(157, 40);
+            this.Reports.TabIndex = 3;
+            this.Reports.Text = "Reports";
+            this.Reports.UseVisualStyleBackColor = false;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.White;
+            this.panel7.BackgroundImage = global::YutMobileStore.Properties.Resources.attendance;
+            this.panel7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel7.Controls.Add(this.Attendance);
+            this.panel7.Location = new System.Drawing.Point(319, 534);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(206, 153);
+            this.panel7.TabIndex = 8;
+            // 
+            // Attendance
+            // 
+            this.Attendance.BackColor = System.Drawing.Color.Black;
+            this.Attendance.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Attendance.FlatAppearance.BorderSize = 0;
+            this.Attendance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Attendance.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Attendance.ForeColor = System.Drawing.Color.White;
+            this.Attendance.Location = new System.Drawing.Point(25, 93);
+            this.Attendance.Name = "Attendance";
+            this.Attendance.Size = new System.Drawing.Size(157, 40);
+            this.Attendance.TabIndex = 3;
+            this.Attendance.Text = "Attendance";
+            this.Attendance.UseVisualStyleBackColor = false;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.White;
+            this.panel6.BackgroundImage = global::YutMobileStore.Properties.Resources.stock;
+            this.panel6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel6.Controls.Add(this.Stock);
+            this.panel6.Location = new System.Drawing.Point(779, 355);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(206, 153);
+            this.panel6.TabIndex = 7;
+            // 
+            // Stock
+            // 
+            this.Stock.BackColor = System.Drawing.Color.Black;
+            this.Stock.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Stock.FlatAppearance.BorderSize = 0;
+            this.Stock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Stock.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stock.ForeColor = System.Drawing.Color.White;
+            this.Stock.Location = new System.Drawing.Point(25, 93);
+            this.Stock.Name = "Stock";
+            this.Stock.Size = new System.Drawing.Size(157, 40);
+            this.Stock.TabIndex = 3;
+            this.Stock.Text = "Edit Stock";
+            this.Stock.UseVisualStyleBackColor = false;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.BackgroundImage = global::YutMobileStore.Properties.Resources.sales;
+            this.panel5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel5.Controls.Add(this.Sale);
+            this.panel5.Location = new System.Drawing.Point(549, 355);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(206, 153);
+            this.panel5.TabIndex = 6;
+            // 
+            // Sale
+            // 
+            this.Sale.BackColor = System.Drawing.Color.Black;
+            this.Sale.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Sale.FlatAppearance.BorderSize = 0;
+            this.Sale.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Sale.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sale.ForeColor = System.Drawing.Color.White;
+            this.Sale.Location = new System.Drawing.Point(25, 93);
+            this.Sale.Name = "Sale";
+            this.Sale.Size = new System.Drawing.Size(157, 40);
+            this.Sale.TabIndex = 3;
+            this.Sale.Text = "Sale";
+            this.Sale.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.BackgroundImage = global::YutMobileStore.Properties.Resources.ledger;
+            this.panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel4.Controls.Add(this.Ledger);
+            this.panel4.Location = new System.Drawing.Point(319, 355);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(206, 153);
+            this.panel4.TabIndex = 5;
+            // 
+            // Ledger
+            // 
+            this.Ledger.BackColor = System.Drawing.Color.Black;
+            this.Ledger.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Ledger.FlatAppearance.BorderSize = 0;
+            this.Ledger.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ledger.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ledger.ForeColor = System.Drawing.Color.White;
+            this.Ledger.Location = new System.Drawing.Point(25, 93);
+            this.Ledger.Name = "Ledger";
+            this.Ledger.Size = new System.Drawing.Size(157, 40);
+            this.Ledger.TabIndex = 3;
+            this.Ledger.Text = "Ledger";
+            this.Ledger.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.BackgroundImage = global::YutMobileStore.Properties.Resources.accessories;
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Controls.Add(this.Accessories);
+            this.panel3.Location = new System.Drawing.Point(779, 176);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(206, 153);
+            this.panel3.TabIndex = 4;
+            // 
+            // Accessories
+            // 
+            this.Accessories.BackColor = System.Drawing.Color.Black;
+            this.Accessories.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Accessories.FlatAppearance.BorderSize = 0;
+            this.Accessories.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Accessories.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Accessories.ForeColor = System.Drawing.Color.White;
+            this.Accessories.Location = new System.Drawing.Point(25, 93);
+            this.Accessories.Name = "Accessories";
+            this.Accessories.Size = new System.Drawing.Size(157, 40);
+            this.Accessories.TabIndex = 3;
+            this.Accessories.Text = "Add Accessories";
+            this.Accessories.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BackgroundImage = global::YutMobileStore.Properties.Resources.phones;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.Phones);
+            this.panel2.Location = new System.Drawing.Point(549, 176);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(206, 153);
+            this.panel2.TabIndex = 3;
+            // 
+            // Phones
+            // 
+            this.Phones.BackColor = System.Drawing.Color.Black;
+            this.Phones.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Phones.FlatAppearance.BorderSize = 0;
+            this.Phones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Phones.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Phones.ForeColor = System.Drawing.Color.White;
+            this.Phones.Location = new System.Drawing.Point(25, 93);
+            this.Phones.Name = "Phones";
+            this.Phones.Size = new System.Drawing.Size(157, 40);
+            this.Phones.TabIndex = 3;
+            this.Phones.Text = "Add Phones";
+            this.Phones.UseVisualStyleBackColor = false;
+            // 
+            // InventoryPanel
+            // 
+            this.InventoryPanel.BackColor = System.Drawing.Color.White;
+            this.InventoryPanel.BackgroundImage = global::YutMobileStore.Properties.Resources.inventory1;
+            this.InventoryPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.InventoryPanel.Controls.Add(this.Inventory);
+            this.InventoryPanel.Location = new System.Drawing.Point(319, 176);
+            this.InventoryPanel.Name = "InventoryPanel";
+            this.InventoryPanel.Size = new System.Drawing.Size(206, 153);
+            this.InventoryPanel.TabIndex = 0;
+            // 
+            // Inventory
+            // 
+            this.Inventory.BackColor = System.Drawing.Color.Black;
+            this.Inventory.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Inventory.FlatAppearance.BorderSize = 0;
+            this.Inventory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Inventory.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Inventory.ForeColor = System.Drawing.Color.White;
+            this.Inventory.Location = new System.Drawing.Point(25, 93);
+            this.Inventory.Name = "Inventory";
+            this.Inventory.Size = new System.Drawing.Size(157, 40);
+            this.Inventory.TabIndex = 3;
+            this.Inventory.Text = "Inventory";
+            this.Inventory.UseVisualStyleBackColor = false;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.LogOut);
+            this.Controls.Add(this.InventoryPanel);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel9.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.InventoryPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel InventoryPanel;
+        private System.Windows.Forms.Button Inventory;
+        private System.Windows.Forms.Button LogOut;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Phones;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button Accessories;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button Ledger;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button Sale;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button Stock;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button Attendance;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Button Reports;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Button ProductInfo;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ YutMobileStore/Login.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace YutMobileStore
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserText = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.Label();
+            this.PassText = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Username = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UserText
+            // 
+            this.UserText.BackColor = System.Drawing.Color.White;
+            this.UserText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UserText.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserText.ForeColor = System.Drawing.Color.Black;
+            this.UserText.Location = new System.Drawing.Point(120, 175);
+            this.UserText.Name = "UserText";
+            this.UserText.Size = new System.Drawing.Size(250, 31);
+            this.UserText.TabIndex = 1;
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Password.Location = new System.Drawing.Point(115, 221);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(106, 26);
+            this.Password.TabIndex = 0;
+            this.Password.Text = "Password:";
+            // 
+            // PassText
+            // 
+            this.PassText.BackColor = System.Drawing.Color.White;
+            this.PassText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PassText.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PassText.ForeColor = System.Drawing.Color.Black;
+            this.PassText.Location = new System.Drawing.Point(120, 269);
+            this.PassText.Name = "PassText";
+            this.PassText.Size = new System.Drawing.Size(250, 31);
+            this.PassText.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.Controls.Add(this.Username);
+            this.panel1.Controls.Add(this.LoginButton);
+            this.panel1.Controls.Add(this.UserText);
+            this.panel1.Controls.Add(this.PassText);
+            this.panel1.Controls.Add(this.Password);
+            this.panel1.Location = new System.Drawing.Point(441, 137);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(485, 465);
+            this.panel1.TabIndex = 3;
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Username.Location = new System.Drawing.Point(115, 130);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(109, 26);
+            this.Username.TabIndex = 4;
+            this.Username.Text = "Username:";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.Color.Black;
+            this.LoginButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.LoginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginButton.Font = new System.Drawing.Font("Candara", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.LoginButton.Location = new System.Drawing.Point(120, 337);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(250, 41);
+            this.LoginButton.TabIndex = 2;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.panel1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox UserText;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox PassText;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Button LoginButton;
+    }
+}
+
